78cd193b5a00a45a3db869d68b19e16ccb4f8eaf
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2018-08-23 23:39:32 -0300

    refactor: added license to index and code clean ups.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+#!/usr/bin/env node
+
+/**
+ * @license
+ * Copyright Mauricio Gemelli Vigolo and contributors.
+ *
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file at https://github.com/mauriciovigolo/cdn-keycloak-angular/LICENSE
+ */
+
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -11,24 +21,17 @@ app.use(express.static('public'));
 app.use((req, res) => {
   res.status(404);
 
-  // respond with html page
   if (req.accepts('html')) {
     res.sendFile(path.join(__dirname, 'public/404.html'));
     return;
   }
 
-  // respond with json
   if (req.accepts('json')) {
     res.send({ error: 'Not found' });
     return;
   }
 
-  // default to plain-text. send()
   res.type('txt').send('Not found');
-});
-
-app.use(function(req, res) {
-  res.send('404: Page not Found', 404);
 });
 
 app.listen(4200, () => console.log('App working on 4200'));
